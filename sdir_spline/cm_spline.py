e82ad1b3d56e7c388ad7be002f342a41353c4083
Julian Scholle <Julian.scholle@googlemail.com>
2016-01-29 00:20:38 +0100

    add Catmull-Rom-Spline (1D only)

---
+++ sdir_spline/cm_spline.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+time_ = np.arange(0, 1, 0.01)
+
+points_x = np.array([1.0,-1.5,9.0,8.0])
+
+
+def getGain(p1,p3):
+    return 0.5*(p3-p1)
+
+
+a = np.matrix([[2,-2,1,1],
+               [-3,3,-2,-1],
+               [0,0,1,0],
+               [1, 0, 0, 0]
+               ])
+
+
+
+#for i in time_:
+#    d.append(float(time(i)*a*np.transpose(points_x)))
+
+
+
+def calcGains(points):
+    gain=list()
+    for i in range(0,len(points)):
+        if i==0:
+            gain.append(points[0]*2.0)
+        elif i==len(points)-1:
+            gain.append(points[i]*2)
+        else:
+            gain.append(getGain(points[i-1],points[i+1]))
+    return gain
+
+
+def calcValue(points):
+    d=list()
+    gains = calcGains(points)
+    print(gains)
+    print()
+    for i in range(0,len(points)-1):
+        C = np.transpose(np.matrix([points[i],points[i+1],gains[i],gains[i+1]]))
+        for t in time_:
+            d.append(float(np.matrix([t**3,t**2,t,1])*a*C))
+    return d
+
+
+
+
+
+
+data = calcValue(points_x)
+
+print(len(np.arange(0, 3, 0.01)))
+
+
+plt.plot(np.arange(0, 3, 0.01),data)
+plt.plot(np.arange(0, 4, 1),points_x,'ro')
+plt.ylabel('some numbers')
+plt.show()
